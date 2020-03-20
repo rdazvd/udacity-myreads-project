65a0cf8ff20fe8768c2dd68daccd19d899433717
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-20 22:18:11 +0000

    feat: created SearchPage class component

---
+++ src/SearchPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class SearchPage extends React.Component {
+  state = {
+    query: ''
+  };
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author"/>
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchPage;
