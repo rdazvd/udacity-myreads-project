9a71369e6287f8a3507496070bd2345585e2b20c
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-20 18:20:40 +0000

    feat: created BookList functional component

---
+++ src/BookList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BookShelf from './BookShelf';
+
+const BookList = ({ books }) {
+  const shelves = [ ...new Set(books.map(book => book.shelf)) ];
+  const booksByShelf = shelves.map(shelf => books.filter(book => book.shelf === shelf));
+
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+        { booksByShelf.map((shelf, shelfTitle) =>
+            <BookShelf shelfTitle={shelves[shelfTitle]} books={shelf} />) }
+      </div>
+      <div className="open-search">
+        <a onClick={() => {}}>Add a book</a>
+      </div>
+    </div>
+  );
+}
+
+BookList.propTypes = {
+  books: PropTypes.array.isRequired
+};
+
+export default BookList;
