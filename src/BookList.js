33cd66ffe73feaa48f02dbca4a683efe25553e90
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-20 21:42:20 +0000

    fix: arrow was missing from BookList component function

--- src/BookList.js
+++ src/BookList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BookShelf from './BookShelf';
 
-const BookList = ({ books }) {
+const BookList = ({ books }) => {
   const shelves = [ ...new Set(books.map(book => book.shelf)) ];
   const booksByShelf = shelves.map(shelf => books.filter(book => book.shelf === shelf));
 
@@ -18,7 +18,7 @@ const BookList = ({ books }) {
       </div>
     </div>
   );
-}
+};
 
 BookList.propTypes = {
   books: PropTypes.array.isRequired
