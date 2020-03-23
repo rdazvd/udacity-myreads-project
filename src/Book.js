7016a5298ba39f0832479d1510dee3fa4e6226c6
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-23 18:04:16 +0000

    refactor: use ShelfChanger in Book replaced book-shelf-changer div with an instance of ShelfChanger

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
+import ShelfChanger from './ShelfChanger';
+
+const Book = ({ book, shelves }) => (
   <li>
     <div className="book">
       <div className="book-top">
@@ -14,15 +16,7 @@ const Book = ({ book }) => (
           }}
         >
         </div>
-        <div className="book-shelf-changer">
-          <select>
-            <option value="move" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
-        </div>
+        <ShelfChanger shelves={shelves} />
       </div>
       <div className="book-title">{book.title}</div>
       <div className="book-authors">{book.authors}</div>
