5f4b1cb54e806d688f4a051fe36125e19053d801
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-23 14:24:26 +0000

    fix: key prop added to Book instantiations in BookShelf.js

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -7,7 +7,7 @@ const BookShelf = ({ shelfTitle, books }) => (
     <h2 className="bookshelf-title">{ shelfTitle }</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
-       { books.map(book => <Book book={book} />) }
+       { books.map(book => <Book key={book.id} book={book} />) }
       </ol>
     </div>
   </div>
