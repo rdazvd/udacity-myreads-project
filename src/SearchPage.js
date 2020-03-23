021bfdd2939f576305dc7e26e70d66a6093d0620
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-23 15:50:13 +0000

    Refactor: added Proptypes declaration to SearchPage.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import Book from './Book';
 
 class SearchPage extends React.Component {
+  static propTypes = {
+    search: PropTypes.func.isRequired
+  };
+
   state = {
     query: '',
     queryResult: [],
