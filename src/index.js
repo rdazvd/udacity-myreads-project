04909e950a1b1179e74d7988746ad416a969f686
rdazvd <29780126+rdazvd@users.noreply.github.com>
2020-03-24 22:02:17 +0000

    chore: insert semicolons in index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
@@ -9,4 +9,4 @@ ReactDOM.render(
     <App />
   </BrowserRouter>, 
   document.getElementById('root')
-)
+);
